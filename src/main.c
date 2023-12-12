a5ba991d697926536bb19967ddb3f4268e84afdc
rhuibertsjr <rhuibertsjr@gmail.com>
2023-12-12 11:29:34 +0000

    Added: assignment template

--- src/main.c
+++ src/main.c
@@ -1,4 +1,57 @@
+#include "main.h" 
+
 #include <stdio.h>
+
+void * 
+task_one(void)
+{
+  while(1)
+  {
+    printf("Task 1 \n");
+    for (u64 m = 0, n = 0; m <= 1000; m++)
+    {
+      for (;n <= 10000; n++);
+    }
+  }
+}
+
+void * 
+task_two(void)
+{
+  while(1)
+  {
+    printf("Task 2 \n");
+    for (u64 m = 0, n = 0; m <= 1000; m++)
+    {
+      for (;n <= 10000; n++);
+    }
+  }
+}
+
+void * 
+task_three(void)
+{
+  while(1)
+  {
+    printf("Task 3 \n");
+    for (u64 m = 0, n = 0; m <= 1000; m++)
+    {
+      for (;n <= 10000; n++);
+    }
+  }
+}
+void * 
+task_four(void)
+{
+  while(1)
+  {
+    printf("Task 4 \n");
+    for (u64 m = 0, n = 0; m <= 1000; m++)
+    {
+      for (;n <= 10000; n++);
+    }
+  }
+}
 
 int main(void) 
 {
